11d95f8671258c44f810e56b71d7649f47ff6d60
6Morpheus6 <wanton-decay@gmx.net>
2025-12-26 22:54:44 +0100

    update
    - reset confirmation window
    - save disk space button

--- pinokio.js
+++ pinokio.js
@@ -9,7 +9,8 @@ module.exports = {
       install: info.running("install.js"),
       start: info.running("start.js"),
       update: info.running("update.js"),
-      reset: info.running("reset.js")
+      reset: info.running("reset.js"),
+      link: info.running("link.js")
     }
     if (running.install) {
       return [{
@@ -28,14 +29,14 @@ module.exports = {
             text: "Open Web UI",
             href: local.url,
           }, {
-            icon: "fa-solid fa-terminal",
+            icon: 'fa-solid fa-terminal',
             text: "Terminal",
             href: "start.js",
           }]
         } else {
           return [{
             default: true,
-            icon: "fa-solid fa-terminal",
+            icon: 'fa-solid fa-terminal',
             text: "Terminal",
             href: "start.js",
           }]
@@ -43,16 +44,23 @@ module.exports = {
       } else if (running.update) {
         return [{
           default: true,
-          icon: "fa-solid fa-terminal",
+          icon: 'fa-solid fa-terminal',
           text: "Updating",
           href: "update.js",
         }]
       } else if (running.reset) {
         return [{
           default: true,
-          icon: "fa-solid fa-terminal",
+          icon: 'fa-solid fa-terminal',
           text: "Resetting",
           href: "reset.js",
+        }]
+      } else if (running.link) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Deduplicating",
+          href: "link.js",
         }]
       } else {
         return [{
@@ -65,9 +73,18 @@ module.exports = {
           text: "Update",
           href: "update.js",
         }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-solid fa-file-zipper",
+          text: "<div><strong>Save Disk Space</strong><div>Deduplicates redundant library files</div></div>",
+          href: "link.js",
+        }, {
           icon: "fa-regular fa-circle-xmark",
-          text: "Reset",
+          text: "<div><strong>Reset</strong><div>Revert to pre-install state</div></div>",
           href: "reset.js",
+          confirm: "Are you sure you wish to reset the app?"
         }]
       }
     } else {
